0e290cf4be7b80897b170fd189401b5f60bad86e
Matt Heller <mheller42@gmail.com>
2012-08-25 13:02:19 -0400

    sadfasdfsadf

--- PantsPartyApp/gen/com/tres/pantsparty/R.java
+++ PantsPartyApp/gen/com/tres/pantsparty/R.java
@@ -18,36 +18,36 @@ public final class R {
         public static final int video_thumb=0x7f020004;
     }
     public static final class id {
-        public static final int VideoTitle=0x7f07001a;
-        public static final int btnDoucheMode=0x7f070003;
-        public static final int btnExit=0x7f070007;
-        public static final int btnLearnToParty=0x7f070004;
-        public static final int btnPantsParty=0x7f070006;
-        public static final int btnRealUltimateParty=0x7f070005;
-        public static final int btnStartParty=0x7f070002;
-        public static final int feature_1=0x7f070008;
-        public static final int feature_2=0x7f07000b;
-        public static final int feature_3=0x7f07000e;
-        public static final int feature_4=0x7f070011;
-        public static final int feature_5=0x7f070014;
-        public static final int feature_6=0x7f070017;
-        public static final int feature_icon_1=0x7f070009;
-        public static final int feature_icon_2=0x7f07000c;
-        public static final int feature_icon_3=0x7f07000f;
-        public static final int feature_icon_4=0x7f070012;
-        public static final int feature_icon_5=0x7f070015;
-        public static final int feature_icon_6=0x7f070018;
-        public static final int feature_name_1=0x7f07000a;
-        public static final int feature_name_2=0x7f07000d;
-        public static final int feature_name_3=0x7f070010;
-        public static final int feature_name_4=0x7f070013;
-        public static final int feature_name_5=0x7f070016;
-        public static final int feature_name_6=0x7f070019;
-        public static final int menu_settings=0x7f07001d;
-        public static final int quickContactBadge1=0x7f070001;
-        public static final int textView1=0x7f070000;
-        public static final int vidDescription=0x7f07001c;
-        public static final int videoThumb=0x7f07001b;
+        public static final int VideoTitle=0x7f08001a;
+        public static final int btnDoucheMode=0x7f080003;
+        public static final int btnExit=0x7f080007;
+        public static final int btnLearnToParty=0x7f080004;
+        public static final int btnPantsParty=0x7f080006;
+        public static final int btnRealUltimateParty=0x7f080005;
+        public static final int btnStartParty=0x7f080002;
+        public static final int feature_1=0x7f080008;
+        public static final int feature_2=0x7f08000b;
+        public static final int feature_3=0x7f08000e;
+        public static final int feature_4=0x7f080011;
+        public static final int feature_5=0x7f080014;
+        public static final int feature_6=0x7f080017;
+        public static final int feature_icon_1=0x7f080009;
+        public static final int feature_icon_2=0x7f08000c;
+        public static final int feature_icon_3=0x7f08000f;
+        public static final int feature_icon_4=0x7f080012;
+        public static final int feature_icon_5=0x7f080015;
+        public static final int feature_icon_6=0x7f080018;
+        public static final int feature_name_1=0x7f08000a;
+        public static final int feature_name_2=0x7f08000d;
+        public static final int feature_name_3=0x7f080010;
+        public static final int feature_name_4=0x7f080013;
+        public static final int feature_name_5=0x7f080016;
+        public static final int feature_name_6=0x7f080019;
+        public static final int menu_settings=0x7f08001d;
+        public static final int quickContactBadge1=0x7f080001;
+        public static final int textView1=0x7f080000;
+        public static final int vidDescription=0x7f08001c;
+        public static final int videoThumb=0x7f08001b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -55,27 +55,32 @@ public final class R {
         public static final int video_info_generic=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
+    }
+    public static final class raw {
+        public static final int blockinbeats=0x7f040000;
+        public static final int dontforgetaboutme=0x7f040001;
+        public static final int rebelyell=0x7f040002;
     }
     public static final class string {
-        public static final int DoucheMode=0x7f040006;
-        public static final int LearnToParty=0x7f040005;
-        public static final int PantsParty=0x7f04000e;
-        public static final int RealUltimateParty=0x7f04000d;
-        public static final int StartParty=0x7f040004;
-        public static final int app_name=0x7f040000;
-        public static final int exit=0x7f04000f;
-        public static final int feature_1=0x7f040009;
-        public static final int feature_2=0x7f04000a;
-        public static final int feature_3=0x7f04000b;
-        public static final int feature_4=0x7f04000c;
-        public static final int feature_5=0x7f040007;
-        public static final int feature_6=0x7f040008;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        public static final int DoucheMode=0x7f050006;
+        public static final int LearnToParty=0x7f050005;
+        public static final int PantsParty=0x7f05000e;
+        public static final int RealUltimateParty=0x7f05000d;
+        public static final int StartParty=0x7f050004;
+        public static final int app_name=0x7f050000;
+        public static final int exit=0x7f05000f;
+        public static final int feature_1=0x7f050009;
+        public static final int feature_2=0x7f05000a;
+        public static final int feature_3=0x7f05000b;
+        public static final int feature_4=0x7f05000c;
+        public static final int feature_5=0x7f050007;
+        public static final int feature_6=0x7f050008;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int title_activity_main=0x7f050003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
