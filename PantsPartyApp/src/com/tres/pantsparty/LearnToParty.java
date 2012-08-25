ecf3100e6f20dd1246d9d2c64ce426f08bce8e6e
Matt Heller <mheller42@gmail.com>
2012-08-25 11:31:12 -0400

    Commit 1

---
+++ PantsPartyApp/src/com/tres/pantsparty/LearnToParty.java
@@ -0,0 +1,106 @@
+/*
+ ******************************************************************************
+ * Parts of this code sample are licensed under Apache License, Version 2.0   *
+ * Copyright (c) 2009, Android Open Handset Alliance. All rights reserved.    *
+ *                                                                            *                                                                         *
+ * Except as noted, this code sample is offered under a modified BSD license. *
+ * Copyright (C) 2010, Motorola Mobility, Inc. All rights reserved.           *
+ *                                                                            *
+ * For more details, see MOTODEV_Studio_for_Android_LicenseNotices.pdf        * 
+ * in your installation folder.                                               *
+ ******************************************************************************
+ */
+
+package com.tres.pantsparty;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class LearnToParty extends Activity {
+
+	private ViewGroup feature_1;
+	private ViewGroup feature_2;
+	private ViewGroup feature_3;
+	private ViewGroup feature_4;
+	private ViewGroup feature_5;
+	private ViewGroup feature_6;
+
+	/**
+	 * @see android.app.Activity#onCreate(Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dashboard);
+
+		this.feature_1 = (ViewGroup) findViewById(R.id.feature_1);
+		this.feature_2 = (ViewGroup) findViewById(R.id.feature_2);
+		this.feature_3 = (ViewGroup) findViewById(R.id.feature_3);
+		this.feature_4 = (ViewGroup) findViewById(R.id.feature_4);
+		this.feature_5 = (ViewGroup) findViewById(R.id.feature_5);
+		this.feature_6 = (ViewGroup) findViewById(R.id.feature_6);
+
+		View.OnClickListener onClickListener = new DashboardClickListener();
+
+		this.feature_1.setOnClickListener(onClickListener);
+		this.feature_2.setOnClickListener(onClickListener);
+		this.feature_3.setOnClickListener(onClickListener);
+		this.feature_4.setOnClickListener(onClickListener);
+		this.feature_5.setOnClickListener(onClickListener);
+		this.feature_6.setOnClickListener(onClickListener);
+
+	}
+
+	private class DashboardClickListener implements View.OnClickListener {
+
+		public void onClick(View v) {
+
+			/*
+			 * TODO: Replace the code below with your business logic
+			 * 
+			 * You will probably open an activity, which can be done using a
+			 * code like below: startActivity(new Intent(v.getContext(),
+			 * YourActivity.class));
+			 */
+
+			String msg = "You selected Feature ";
+
+			switch (v.getId()) {
+			case R.id.feature_1:
+				msg += "1";
+				startVideoInfoGeneric();
+				break;
+			case R.id.feature_2:
+				msg += "2";
+				break;
+			case R.id.feature_3:
+				msg += "3";
+				break;
+			case R.id.feature_4:
+				msg += "4";
+				break;
+			case R.id.feature_5:
+				msg += "5";
+				break;
+			case R.id.feature_6:
+				msg += "6";
+				break;
+			default:
+				// none
+			}
+
+			Toast.makeText(v.getContext(), msg, Toast.LENGTH_SHORT).show();
+		}
+
+		private void startVideoInfoGeneric() {
+			Intent i = new Intent(this, VideoInfoGeneric.class);
+			startActivity(i);
+		}
+
+	}
+
+}
--- PantsPartyApp/src/com/tres/pantsparty/MainActivity.java
+++ PantsPartyApp/src/com/tres/pantsparty/MainActivity.java
@@ -1,15 +1,39 @@
 package com.tres.pantsparty;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	private Button mStartParty;
+	private Button mDoucheMode;
+	private Button mLearnToParty;
+	private Button mExit;
+	private Button mPantsParty;
+	private Button mRealUltimateParty;
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mStartParty = (Button) findViewById(R.id.btnStartParty);
+        mDoucheMode = (Button) findViewById(R.id.btnDoucheMode);
+        mLearnToParty = (Button) findViewById(R.id.btnLearnToParty);
+        mPantsParty = (Button) findViewById(R.id.btnPantsParty);
+        mRealUltimateParty = (Button) findViewById(R.id.btnRealUltimateParty);
+
+        mStartParty.setOnClickListener(this);
+        mDoucheMode.setOnClickListener(this);
+        mLearnToParty.setOnClickListener(this);
+        mPantsParty.setOnClickListener(this);
+        mRealUltimateParty.setOnClickListener(this);
     }
 
     @Override
@@ -17,4 +41,51 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btnDoucheMode:
+			startDoucheMode();
+			break;
+		case R.id.btnLearnToParty:
+			startLearnToParty();
+			break;
+		case R.id.btnStartParty:
+			startStartParty();
+			break;
+		case R.id.btnPantsParty:
+			startPantsParty();
+			break;
+		case R.id.btnRealUltimateParty:
+			startRealUltimateParty();
+			break;		
+		}
+	}
+
+	private void startPantsParty() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void startRealUltimateParty() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void startStartParty() {
+		//int myInt = 0;
+		//Intent i = new Intent(this, StartParty.class);
+		//startActivityForResult(i, myInt);
+	}
+
+	private void startLearnToParty() {
+		Intent i = new Intent(this, LearnToParty.class);
+		startActivity(i);
+	}
+
+	private void startDoucheMode() {
+		// TODO Auto-generated method stub
+		
+	}
 }
